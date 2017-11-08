225b2716d11404a6e31ab6dac597bdad9429a1f7
Gerard Mulder <g.mulder@cgi.com>
2017-11-08 15:34:22 +0100

    Added Eureka

--- src/main/java/nl/kadaster/sensor/identity/IdentityApplication.java
+++ src/main/java/nl/kadaster/sensor/identity/IdentityApplication.java
@@ -2,7 +2,9 @@ package nl.kadaster.sensor.identity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+	@EnableDiscoveryClient
 @SpringBootApplication
 public class IdentityApplication {
 
