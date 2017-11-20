f6b3f753f3dcd8aa58da2f342f0025ede58bb520
Raymond Kroon <raymond@k3n.nl>
2017-11-20 17:51:45 +0100

    sensor with code value and status

--- src/main/java/nl/kadaster/sensor/register/SensorRepository.java
+++ src/main/java/nl/kadaster/sensor/register/SensorRepository.java
@@ -1,8 +1,22 @@
 package nl.kadaster.sensor.register;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "sensors", path = "sensors")
 public interface SensorRepository extends PagingAndSortingRepository<Sensor, Long> {
+
+    SensorWithCodeValue findOneByCode_ValueAndCode_StatusIs(@Param("value") String value, @Param("status") Status status);
+
+
+    public static interface SensorWithCodeValue {
+        String getName();
+        String getDescription();
+        CodeValue getCode();
+
+        public static interface CodeValue {
+            String getValue();
+        }
+    }
 }
