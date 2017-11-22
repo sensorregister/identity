2b4d7f35014fe366a0f0f64caa37b0f6e2dd42fd
Raymond Kroon <raymond@k3n.nl>
2017-11-22 11:39:12 +0100

    fix sensor results

--- src/main/java/nl/kadaster/sensor/register/SensorRepository.java
+++ src/main/java/nl/kadaster/sensor/register/SensorRepository.java
@@ -3,20 +3,20 @@ package nl.kadaster.sensor.register;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.config.Projection;
 
 @RepositoryRestResource(collectionResourceRel = "sensors", path = "sensors")
 public interface SensorRepository extends PagingAndSortingRepository<Sensor, Long> {
 
-    SensorWithCodeValue findOneByCode_ValueAndCode_StatusIs(@Param("value") String value, @Param("status") Status status);
+    Sensor findOneByCode_ValueAndCode_StatusIs(@Param("value") String value, @Param("status") Status status);
 
 
-    public static interface SensorWithCodeValue {
+    @Projection(name = "withCode", types = Sensor.class)
+    interface SensorWithCode {
+
         String getName();
         String getDescription();
-        CodeValue getCode();
+        Code getCode();
 
-        public static interface CodeValue {
-            String getValue();
-        }
     }
 }
